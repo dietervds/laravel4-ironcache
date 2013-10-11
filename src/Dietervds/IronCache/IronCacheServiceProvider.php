7eb413ff4033f9e2edfcce4ae1c94d40a736267a
Dieter Van der Stock <dietervds@gmail.com>
2013-10-11 15:50:22 +0300

    Using a config file

--- src/Dietervds/IronCache/IronCacheServiceProvider.php
+++ src/Dietervds/IronCache/IronCacheServiceProvider.php
@@ -5,8 +5,18 @@ use Illuminate\Cache\Repository;
 
 class IronCacheServiceProvider extends ServiceProvider {
 
+    /**
+     * Indicates if loading of the provider is deferred.
+     *
+     * @var bool
+     */
+    protected $defer = false;
+    
+
     public function boot()
     {
+        $this->package('dietervds/laravel4-ironcache');
+
         $this->app['cache']->extend('ironcache', function($app)
         {
             return new Repository(new IroncacheStore);
---
+++ src/config/config.php
@@ -0,0 +1,34 @@
+<?php 
+
+return array( 
+    
+   /*
+    |--------------------------------------------------------------------------
+    | IronCache config
+    |--------------------------------------------------------------------------
+    |
+    | To find your token and project_id: 
+    |   - Log into your Iron.io dashboard at https://hud.iron.io/
+    |   - You will see your projects (if you don't have any, create one)
+    |   - Click on the 'key' icon (='credentials') of your project. There you will see what you need.
+    |
+    | The name for your cache is something that you choose yourself. Just pick whatever makes sense.
+    |
+    */
+
+    /**
+     * Iron.io token for this project
+     */
+    'token' => '', 
+
+    /**
+     * Iron.io project id
+     */
+    'project_id' => '', 
+
+    /**
+     * Name for the Iron.io cache that you want to use
+     */
+    'cache_name' => '', 
+
+);
