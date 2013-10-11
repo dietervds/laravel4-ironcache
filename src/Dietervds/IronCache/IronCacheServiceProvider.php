d0885181185a9c787644ef2e571ba5108e95964e
Dieter Van der Stock <dietervds@gmail.com>
2013-10-11 14:48:33 +0300

    Added IronCacheSTore

--- src/Dietervds/IronCache/IronCacheServiceProvider.php
+++ src/Dietervds/IronCache/IronCacheServiceProvider.php
@@ -5,11 +5,16 @@ use Illuminate\Cache\Repository;
 
 class IronCacheServiceProvider extends ServiceProvider {
 
-    public function register()
+    public function boot()
     {
         $this->app['cache']->extend('ironcache', function($app)
         {
             return new Repository(new IroncacheStore);
         });
+    }
+
+    public function register()
+    {
+
     }
 }
